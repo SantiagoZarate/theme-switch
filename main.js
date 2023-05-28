9b68cdb61a9b5160632729f2f85da6464d47ef61
Santiago Zarate <santiago2001zarate@gmail.com>
2023-05-27 21:04:22 -0300

    Añadido local storage, ahora la pagina recuerda el ultima theme usado, falta mejorar el tema del icono del boton al guardarse en local storage

--- main.js
+++ main.js
@@ -3,9 +3,20 @@ const buttonElement = document.getElementById("theme-button")
 const iconElement = document.querySelector(".container__footer__theme__button");
 const icono = document.querySelector(".icono");
 
+// CHECKO SI YA EXISTE UN THEME EN LOCAL STORAGE
+bodyElement.classList.add(localStorage.getItem("theme"));
+
+
 //EVENT LISTENERS
 buttonElement.addEventListener("click", function(){
-    bodyElement.classList.toggle("light-mode")
+    bodyElement.classList.toggle("light-mode");
+    console.log("no entre")
+    if(localStorage.getItem("theme") == null){
+        localStorage.setItem("theme", "light-mode");
+    }else{
+        localStorage.removeItem("theme");
+    }
+
     iconElement.classList.toggle("theme--button--pressed");
 
     if(!icono.classList.contains("light--theme")){
